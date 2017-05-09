e29264525890d57d352c05cd29014673156820ba
Xingyu Wang <xingyuwang@dhcp-10-105-145-16.wireless.northwestern.private>
2017-05-08 20:15:51 -0500

    QAP final version

--- Rpreliminary/QAP.R
+++ Rpreliminary/QAP.R
@@ -23,15 +23,17 @@ pol = pol[pol$Source!=pol$website,]
 #fake news
 #g_valued <- graph_from_data_frame(d = network_table[,1:3,with=FALSE],directed = FALSE,vertices = total_table)
 pol_fake = pol[,c(1,2,5)]
-pol_fake = pol_fake[pol_fake$FALSE.>0,]
+#pol_fake = pol_fake[pol_fake$FALSE.>0,]
 g_fake_valued = graph_from_data_frame(d = pol_fake,directed = T)
 am_fake_valued = as_adj(g_fake_valued)
+m_fake_valued = as.matrix(am_fake_valued)
 
 #real news
 pol_true = pol[,c(1,2,4)]
-pol_true = pol_true[pol_true$TRUE.>0,]
+#pol_true = pol_true[pol_true$TRUE.>0,]
 g_real_valued = graph_from_data_frame(d = pol_true,directed = T)
 am_real_valued = as_adj(g_real_valued)
+m_real_valued = as.matrix(am_real_valued)
 
 #graph.density(g_fake_valued)
 #L = layout_with_fr(g_fake_valued)  # Fruchterman Reingold
@@ -42,9 +44,18 @@ centr_degree(g_fake_valued,mode='all',loops = FALSE,normalized = TRUE)
 centr_degree(g_real_valued,mode='all',loops = FALSE,normalized = TRUE)
 
 
+
+
+
+#############
+#QAP
+#############
+
 detach(package:igraph) 
 library('sna')
 
-qapoutput = qaptest(list(g_fake_valued,g_real_valued),gcor,g1=1,g2=2,reps=1000) 
+g_fake_binary_net = network(m_fake_valued,directed = T)
+g_real_binary_net = network(m_real_valued,directed = T)
 
+qapoutput = qaptest(list(g_fake_binary_net,g_real_binary_net),gcor,g1=1,g2=2,reps=1000) 
 
